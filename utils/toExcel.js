526bd52c7c3f6873b0e7c2125ac4e157f58b6628
Darío Rivera <fermius.us@gmail.com>
2017-10-06 09:41:40 -0500

    Improve charset on Windows

--- utils/toExcel.js
+++ utils/toExcel.js
@@ -64,7 +64,7 @@ JScriptRender.utils.toExcel = function(node)
     var data = node.outerHTML;
 
     var uri      = 'data:application/vnd.ms-excel;base64,';
-    var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office"xmlns:x="urn:schemas-microsoft-com:office:excel"xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--></head><body>{table}</body></html>';
+    var template = '<html xmlns:o="urn:schemas-microsoft-com:office:office"xmlns:x="urn:schemas-microsoft-com:office:excel"xmlns="http://www.w3.org/TR/REC-html40"><head><!--[if gte mso 9]><xml><x:ExcelWorkbook><x:ExcelWorksheets><x:ExcelWorksheet><x:Name>{worksheet}</x:Name><x:WorksheetOptions><x:DisplayGridlines/></x:WorksheetOptions></x:ExcelWorksheet></x:ExcelWorksheets></x:ExcelWorkbook></xml><![endif]--><meta http-equiv="content-type" content="application/vnd.ms-excel; charset=UTF-8"></head><body>{table}</body></html>';
 
     var base64_encode   = function (s) { return window.btoa(unescape(encodeURIComponent(s))) };
     var format_template = function (s, c) { return s.replace(/{(\w+)}/g, function (m, p) { return c[p]; }) }
